4d3853de82ef63aa378c94ec5ef035d029f5a76e
Gianluca-V <vespe.gianluca@gmail.com>
2023-11-14 01:29:29 -0300

    added description and image

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@ import { WeatherAPI } from "./wheater.js";
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const data = await WeatherAPI.GetData();
+        console.log(data);
         const temperatureMin = data.forecast.forecastday[0].day.maxtemp_c;
         const temperatureMax = data.forecast.forecastday[0].day.mintemp_c;
         const temperatureMinElement = document.querySelector(".temperature__min");
         const temperatureMaxElement = document.querySelector(".temperature__max");
         temperatureMaxElement.textContent = temperatureMax;
         temperatureMinElement.textContent = temperatureMin;
+        const temperatureConditionElement = document.querySelector(".temperature__condition");
+        const temperatureCondition = data.forecast.forecastday[0].day.condition.text;
+        temperatureConditionElement.textContent = temperatureCondition;
+        const iconElement = document.querySelector(".weather-icon__image");
+        const icon = data.forecast.forecastday[0].day.condition.icon;
+        iconElement.setAttribute("src", icon);
     }
     catch (error) {
         console.error(error);
